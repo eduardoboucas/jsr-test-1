2f9b288fa060b3a1a251fa3f158325aff86b6603
Eduardo Bouças <mail@eduardoboucas.com>
2024-12-12 20:28:10 +0000

    Test

--- mod.ts
+++ mod.ts
@@ -1,10 +1,3 @@
-import cowsay from "npm:cowsay@^1.6.0";
+import { moo } from "jsr:@eduardoboucas/jsr-test-2";
 
-export const greet = (name: string): string => `Hello, ${name}`;
-
-export const moo = (name: string): string =>
-  cowsay.say({
-    text: name,
-    e: "oO",
-    T: "U "
-  });
+export const greet = (name: string): string => moo(name);
